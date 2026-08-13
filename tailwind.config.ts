bd1a164efd6b0c70ada03bad2dbd618e086a3a4e
Swapnil Kuri <99502603+swapnilkuri@users.noreply.github.com>
2026-08-14 01:51:50 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,24 @@
+import type { Config } from "tailwindcss";
 
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          deep: "#0f172a",
+          accent: "#0284c7",
+          ice: "#38bdf8",
+          card: "#1e293b",
+          border: "#334155"
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
